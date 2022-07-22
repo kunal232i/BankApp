e3d0c626cbadbf0d9eaa5e71fbafd04c8999ff5f
kunal232i <kingawale23@gmail.com>
2022-07-22 20:23:48 +0000

    Final Change 👍

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+const depo = document.getElementById("deposite");
+const depoInput = document.getElementById("depoInput");
+const draw = document.getElementById("withdraw");
+const drawInput = document.getElementById("drawInput");
+const bal = document.getElementById("balance");
+
 class Bank {
-  constructor(name) {
-    this.name = name;
+  constructor() {
     this.balance = 0;
   }
 
   deposite(money) {
-    this.balance += money;
+    this.balance += Number(money);
     console.log(`Balance: ${this.balance}`);
+    bal.innerHTML = `<div>₹${this.balance}</div>`;
   }
   withdraw(money) {
-    this.balance -= money;
+    this.balance -= Number(money);
+
     console.log(`Balance: ${this.balance}`);
+    bal.innerHTML = `<div>₹${this.balance}</div>`;
   }
 }
 
-const Kunal = new Bank("kunal");
+const Kunal = new Bank();
 console.log(Kunal);
-Kunal.deposite(100);
-Kunal.withdraw(30);
+
+depo.onclick = () => Kunal.deposite(depoInput.value);
+draw.onclick = () => Kunal.withdraw(drawInput.value);
